83ecb29aa152a14d4360554024ae8dd93064425b
ag-Akshat10 <agakshat01@gmail.com>
2024-07-04 14:58:49 +0530

    change left side of login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
+    const user="";
+    const {
+        Register,
+        handleSubmit,
+        formState:{errors},
+    }=useForm();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        user && navigate("/dashboard");
+    },[user]);
+
   return (
-    <div>
-        Login
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+       <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+           <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+               <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5
+               md:gap-y-10 2xl:-mt-20">
+                   <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600">
+                       Manage all your task in one place!
+                   </span>
+               </div>
+
+           </div>
+           <div className=""></div>
+       </div>
     </div>
   );
 }
