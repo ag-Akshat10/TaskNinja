2ca000114789eee285d3e5211782278ef40f634d
ag-Akshat10 <agakshat01@gmail.com>
2024-07-04 06:49:21 +0530

    Vite and tailwind css setup

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+import Login from "./pages/Login.jsx";
+
+function App() {
+
+
+  return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+  )
+
+
+}
+
+export default App
